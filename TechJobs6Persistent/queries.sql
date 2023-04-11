d7ebfbeee8674e5665b3d94c9939572d02b64b37
rodrin19 <65668704+rodrin19@users.noreply.github.com>
2023-04-11 13:58:33 -0500

    -updates sql query

--- TechJobs6Persistent/queries.sql
+++ TechJobs6Persistent/queries.sql
@@ -6,6 +6,8 @@
 --Id, Name, and Employer ID are the columns and their respective data types are Int, long text, int. 
 
 --Part 2: Write a query to list the names of the employers in St. Louis City.
+--SELECT * FROM techjobs.Employers WHERE Location = 'st. louis';
 
 --Part 3: Write a query to return a list of the names and descriptions of all skills that are attached to jobs in alphabetical order.
     --If a skill does not have a job listed, it should not be included in the results of this query.
+    --SELECT * FROM techjobs.JobSkills JOIN techjobs.Jobs ON techjobs.JobSkills.JobsId = techjobs.Jobs.Id JOIN techjobs.Skills ON techjobs.JobSkills.SkillsId = techjobs.Skills.Id ORDER BY techjobs.Jobs.EmployerId ASC, techjobs.Skills.SkillName ASC;
